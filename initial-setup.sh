1c391506b03174e758c4b5aa86a4613baa27e973
Odin <odinsan91@gmail.com>
2019-02-17 11:25:46 -0300

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-sudo apt-get install xorg openbox openbox-menu suckless-tools obmenu lxappearance terminator htop libpam-google-authenticator openssh-server 
+sudo apt-get install xorg openbox openbox-menu suckless-tools obmenu lxappearance terminator htop libpam-google-authenticator openssh-server
+echo "Iniciando configuração do Google Authenticator:"
+google-authenticator
+echo "Configurando o Google Authenticator para SSH"
+echo "  Escrevendo em 'auth required pam_google_authenticator.so' em /etc/pam.d/sshd"
+echo "auth required pam_google_authenticator.so" | sudo tee -a /etc/pam.d/sshd > /dev/null
+echo "  Reescrevendo a configuração de ChallengeResponseAuthentication para yes"
+sudo ex +g/ChallengeResponseAuthentication/d -cwq /etc/ssh/sshd_config
+echo 'ChallengeResponseAuthentication yes' | sudo tee -a /etc/ssh/sshd_config > /dev/null
+echo "Reiniciando o servidor openssh"
+sudo /etc/init.d/ssh restart
