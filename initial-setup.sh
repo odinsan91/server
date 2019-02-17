9e766ee379e47c5318e2a4f2f8865057890c065b
Odin <odinsan91@gmail.com>
2019-02-17 10:51:56 -0300

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install xorg openbox openbox-menu suckless-tools obmenu lxappearance terminator htop libpam-google-authenticator sshd 
+sudo apt-get install xorg openbox openbox-menu suckless-tools obmenu lxappearance terminator htop libpam-google-authenticator openssh-server 
